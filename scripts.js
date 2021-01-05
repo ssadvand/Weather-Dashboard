48857054b7f4084dd6f6a3648d85ea6b22fa3958
ssadvand <ssadvand@uci.edu>
2021-01-04 18:25:39 -0800

    weather bizz

--- scripts.js
+++ scripts.js
@@ -5,21 +5,32 @@ console.log(cityInput);
 
 
 button.addEventListener("click", function (){
-    console.log(cityInput.value)
+    
     thunderPants()
 })
 cityInput.addEventListener("keydown", function (event){
     if(event.key=== "Enter"){
-        console.log(cityInput.value)
+        
         thunderPants()
     }
 })
 function thunderPants(){
-    console.log("puns")
+   var cities = JSON.parse(localStorage.getItem("Cities")? localStorage.getItem("Cities"):"[]");
+    cities.push(cityInput.value)
+    console.log("List: Cities",cities)
+    localStorage.setItem("Cities",JSON.stringify(cities));
+    console.log(cities[0])
+    //console.log(localStorage.getItem("Cities"));
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${API}`
     $.ajax(
        queryURL 
     ).done(function(response){
-        console.log(response)
+        console.log("List: Weather INFO",response)
+        var freedomUnits = ((response.main.temp-273.15)*1.8)+32
+        var windy = (response.wind.speed*2)
+        $(".city-name").text(response.name)
+        $(".temp").text(freedomUnits.toFixed(4))
+        $(".wind").text(windy.toFixed(4))
     })
 }
+
